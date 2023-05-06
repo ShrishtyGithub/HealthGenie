1909b8020c2d52f354499d5cdefe3c3ca8cb7764
Monarch <monarchk7@gmail.com>
2023-05-06 15:47:07 +0530

    updated gitignore

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //initialising whatsapp and qrcode
+
+let response = 'Hello'
+
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
@@ -15,22 +18,63 @@ client.on('ready', () => {
 client.initialize();
 
 
+
 // fetching texts testing
+
+// client.on('message', message => {
+//     console.log(message.body);
+// });
+
 
 client.on('message', message => {
     console.log(message.body);
+
+    if (message.body === '!ping') {
+        message.reply('pong');
+    }
 });
+
+
+//
+
+
 
 
 // openai testing for chatbot
 // const { Configuration, OpenAIApi } = require("openai");
 // const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
+//     apiKey: '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',
 // });
 // const openai = new OpenAIApi(configuration);
-// const response = await openai.createCompletion({
+// const openai_response = openai.createCompletion({
 //     model: "text-davinci-003",
 //     prompt: "Say this is a test",
 //     temperature: 0,
 //     max_tokens: 7,
 // });
+
+
+// import { Configuration, OpenAIApi } from 'openai';
+
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// const basePromptPrefix = "Ask me 10 questions based on- ";
+// const generateAction = async (req, res) => {
+//     // Run first prompt
+//     console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+//     const baseCompletion = await openai.createCompletion({
+//         model: 'text-davinci-003',
+//         prompt: `${basePromptPrefix}${req.body.userInput}`,
+//         temperature: 0.7,
+//         max_tokens: 250,
+//     });
+
+//     const basePromptOutput = baseCompletion.data.choices.pop();
+
+//     res.status(200).json({ output: basePromptOutput });
+// };
